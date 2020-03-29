d520c444d7a076342937d99eb68de62f15c6ca30
huseyingoztok <huseyin.goztok@gmail.com>
2020-03-29 03:08:28 +0300

    Server.js added. Title changed. Code style fixed.

--- vue/src/components/Layout/AppLayout.vue
+++ vue/src/components/Layout/AppLayout.vue
@@ -2,19 +2,18 @@
   <a-layout style="min-height: 100vh" id="components-layout-demo-top" class="layout">
     <app-header />
     <a-layout-content :style="{ padding: '0 50px', marginTop: '100px' }">
-      
-        <div :style="{ background: '#fff', minHeight: '380px' }">
-          <div :style="{ padding: '24px' }">
-            <a-row>
-              <a-col :span="12">
-                <a-input placeholder="Search ..." @change="onSearch">
-                  <a-icon slot="addonAfter" type="search" />
-                </a-input>
-              </a-col>
-            </a-row>
-          </div>
-          <countries-table :search="search"/>
+      <div :style="{ background: '#fff', minHeight: '380px' }">
+        <div :style="{ padding: '24px' }">
+          <a-row>
+            <a-col :span="12">
+              <a-input placeholder="Search ..." @change="onSearch">
+                <a-icon slot="addonAfter" type="search" />
+              </a-input>
+            </a-col>
+          </a-row>
         </div>
+        <countries-table :search="search" />
+      </div>
     </a-layout-content>
     <app-footer />
   </a-layout>
@@ -23,14 +22,8 @@
 <script>
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import CountriesTable from '../../scenes/Countries/CountriesTable';
-import {
-  Layout,
-  Icon,
-  Input,
-  Row,
-  Col
-} from "ant-design-vue";
+import CountriesTable from "../../scenes/Countries/CountriesTable";
+import { Layout, Icon, Input, Row, Col } from "ant-design-vue";
 const { Content } = Layout;
 export default {
   components: {
@@ -53,7 +46,7 @@ export default {
     onSearch(e) {
       this.search = e.target.value.toLowerCase();
     }
-  },
+  }
 };
 </script>
 
